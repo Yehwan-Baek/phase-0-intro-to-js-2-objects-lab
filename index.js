a2b7bba829fae58a11d42afc9e95851d65987553
Yehwan Baek <baekokok98@gmail.com>
2023-03-11 11:43:17 +1100

    added code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Yehwan',
+    streetAddress: '100 Belmore st',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    const newEmployee = {...employee};
+    
+    newEmployee[key] = value;
+
+    return newEmployee;   
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;   
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const deleteKey = {...employee};
+    delete deleteKey[key];
+    return deleteKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
